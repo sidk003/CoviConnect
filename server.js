b4370dd6dc77b5f5e68fc02501877747e1215a76
sidk003 <siddhu1583@gmail.com>
2021-04-10 16:19:56 +0530

    Heroku  deployment

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ dotenv.config({ path: "./.env" });
 
 connecToDB();
 const app = express();
+
+// For Heroku
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build ", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 4000;
 const server = http.createServer(app);
 
